8084664f0ae6f06de77b6442393b242aef3192b3
osuchaya <suchaya.osatis@gmail.com>
2023-05-31 20:21:50 -0700

    Login page added

---
+++ client/src/utils/queries.js
@@ -0,0 +1,32 @@
+import { gql } from '@apollo/client';
+//query = looking for data (gql similar to SQL)
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      firstName
+      lastName
+      email
+      friends {
+        _id
+        username
+        firstName
+        lastName
+      }
+      activities {
+        _id
+        name
+        date
+        location
+      }
+      invites {
+        _id
+      }
+      requests {
+        _id
+      }
+      
+    }
+  }
+`;
