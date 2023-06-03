0d64f22e830afe28e072ab3a6e583fbe26f81dcd
eschindev <118208710+eschindev@users.noreply.github.com>
2023-06-02 18:14:57 -0700

    defined all front-end queries and all existing front-end mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
@@ -12,14 +12,237 @@ export const LOGIN_USER = gql`
   }
 `;
 
-export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
+export const CREATE_USER = gql`
+  mutation createUser($input: UserInput) {
+    createUser(input: $input) {
       token
       user {
-        _id
+        email
         username
+        _id
       }
     }
+  }
+`;
+// {
+//   "input": {
+//     "username": required
+//     "firstName": required
+//     "lastName": required
+//     "email": required
+//     "password": required
+//   }
+// }
+
+export const UPDATE_USER = gql`
+  mutation updateUser($input: UserUpdateInput) {
+    updateUser(input: $input) {
+      username
+      firstName
+      lastName
+      fullName
+      email
+    }
+  }
+`;
+// {
+//   "input": {
+//     "username": not required
+//     "firstName": not required
+//     "lastName": not required
+//     "email": not required
+//     "password": not required
+//   }
+// }
+
+export const DELETE_USER = gql`
+  mutation DeleteUser($id: ID!) {
+    deleteUser(_id: $id)
+  }
+`;
+
+export const CREATE_ACTIVITY = gql`
+  mutation createActivity($input: ActivityInput) {
+    createActivity(input: $input) {
+      _id
+    }
+  }
+`;
+// {
+//   "input": {
+//     "name": required
+//     "description": not required
+//     "date": required
+//     "location": required
+//     "private": required
+//   }
+// }
+
+export const UPDATE_ACTIVITY = gql`
+  mutation updateActivity($id: ID!, $input: ActivityUpdateInput) {
+    updateActivity(_id: $id, input: $input) {
+      _id
+    }
+  }
+`;
+// {
+//   "input": {
+//     "name": not required
+//     "description": not required
+//     "date": not required
+//     "location": not required
+//     "private": not required
+//   }
+// }
+
+export const DELETE_ACTIVITY = gql`
+  mutation deleteActivity($id: ID!) {
+    deleteActivity(_id: $id)
+  }
+`;
+
+export const ADD_COMMENT = gql`
+  mutation addComment($id: ID!, $commentBody: String!) {
+    addComment(_id: $id, commentBody: $commentBody) {
+      comments {
+        _id
+        commentBody
+        user {
+          _id
+          username
+          firstName
+          lastName
+          fullName
+        }
+        username
+        timestamp
+      }
+    }
+  }
+`;
+
+export const DELETE_COMMENT = gql`
+  mutation deleteComment($id: ID!, $commentId: ID!) {
+    deleteComment(_id: $id, commentId: $commentId) {
+      comments {
+        _id
+        commentBody
+        user {
+          _id
+          username
+          firstName
+          lastName
+          fullName
+        }
+      }
+    }
+  }
+`;
+// id argument is for activity id
+
+export const CREATE_INVITE = gql`
+  mutation createInvite($recipient: ID!, $activity: ID!) {
+    createInvite(recipient: $recipient, activity: $activity) {
+      _id
+      sender {
+        _id
+        username
+        firstName
+        lastName
+        fullName
+      }
+      recipient {
+        _id
+        username
+        firstName
+        lastName
+        fullName
+      }
+      status
+    }
+  }
+`;
+
+export const UPDATE_INVITE = gql`
+  mutation updateInvite($id: ID!, $status: String!) {
+    updateInvite(_id: $id, status: $status) {
+      _id
+      sender {
+        _id
+        username
+        firstName
+        lastName
+        fullName
+      }
+      recipient {
+        _id
+        username
+        firstName
+        lastName
+        fullName
+      }
+      activity {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+export const DELETE_INVITE = gql`
+  mutation deleteInvite($id: ID!) {
+    deleteInvite(_id: $id)
+  }
+`;
+
+export const CREATE_REQUEST = gql`
+  mutation createInvite($recipient: ID!) {
+    createRequest(recipient: $recipient) {
+      _id
+      sender {
+        _id
+        username
+        firstName
+        lastName
+        fullName
+      }
+      recipient {
+        _id
+        username
+        firstName
+        lastName
+        fullName
+      }
+      status
+    }
+  }
+`;
+
+export const UPDATE_REQUEST = gql`
+  mutation createInvite($id: ID!, $status: String!) {
+    updateRequest(_id: $id, status: $status) {
+      _id
+      sender {
+        _id
+        username
+        firstName
+        lastName
+        fullName
+      }
+      recipient {
+        _id
+        username
+        firstName
+        lastName
+        fullName
+      }
+      status
+    }
+  }
+`;
+
+export const DELETE_REQUEST = gql`
+  mutation createInvite($id: ID!) {
+    deleteRequest(_id: $id)
   }
 `;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,19 +1,14 @@
 import { gql } from "@apollo/client";
 //query = looking for data (gql similar to SQL)
 export const QUERY_USER = gql`
-  query user($username: String!) {
-    user(username: $username) {
-      _id
+  # get data for your own profile
+  query getMyUser {
+    getMyUser {
       username
       firstName
       lastName
+      fullName
       email
-      friends {
-        _id
-        username
-        firstName
-        lastName
-      }
       activities {
         _id
         name
@@ -21,17 +16,147 @@ export const QUERY_USER = gql`
         location
       }
       invites {
-        _id
+        sender {
+          _id
+          username
+          firstName
+          lastName
+          fullName
+        }
         activity {
-            name
-            date
+          _id
+          name
+          date
+          owner {
+            _id
+            username
+            firstName
+            lastName
+            fullName
+          }
+        }
+      }
+      friends {
+        _id
+        username
+        firstName
+        lastName
+        fullName
+      }
+      requests {
+        sender {
+          _id
+          username
+          firstName
+          lastName
+          fullName
+        }
+      }
+    }
+  }
+
+  # get data for someone else's profile
+  query getUserById($id: ID!) {
+    getUserById(_id: $id) {
+      username
+      firstName
+      lastName
+      fullName
+      email
+      friends {
+        _id
+        username
+        firstName
+        lastName
+        fullName
+      }
+      activities {
+        _id
+        name
+        date
+        location
+      }
+    }
+  }
+
+  # search for users
+  query searchUsers($searchTerm: String!) {
+    searchUsers(searchTerm: $searchTerm) {
+      _id
+      username
+      firstName
+      lastName
+      fullName
+    }
+  }
+
+  # get activity page data
+  query getActivityById($id: ID!) {
+    getActivityById(_id: $id) {
+      name
+      description
+      date
+      location
+      private
+      owner {
+        username
+        firstName
+        lastName
+      }
+      participants {
+        _id
+        username
+        firstName
+        lastName
+        fullName
+      }
+      invites {
+        _id
+        sender {
+          _id
+          username
+          firstName
+          lastName
+          fullName
+        }
+        recipient {
+          _id
+          username
+          firstName
+          lastName
+          fullName
         }
         status
       }
-      requests {
+      comments {
         _id
-        sender
-        status
+        commentBody
+        user {
+          _id
+          username
+          firstName
+          lastName
+          fullName
+        }
+        timestamp
+      }
+    }
+  }
+
+  # search for activities
+  query searchActivities($searchTerm: String!) {
+    searchActivities(searchTerm: $searchTerm) {
+      _id
+      name
+      description
+      date
+      location
+      owner {
+        _id
+        username
+        firstName
+        lastName
+        fullName
       }
     }
   }
