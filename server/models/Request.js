154c6cda490d57010b9511bca6b50ddd616f6068
eschindev <118208710+eschindev@users.noreply.github.com>
2023-06-03 12:12:28 -0700

    resolved circular dependencies, seed.js working properly

--- server/models/Request.js
+++ server/models/Request.js
@@ -1,5 +1,4 @@
 const { Schema, model } = require("mongoose");
-const User = require("./User");
 
 const requestSchema = new Schema({
   sender: {
@@ -18,29 +17,29 @@ const requestSchema = new Schema({
 });
 
 requestSchema.post("save", async function (doc) {
-  await User.findByIdAndUpdate(doc.recipient, {
+  await model("User").findByIdAndUpdate(doc.recipient, {
     $addToSet: { requests: doc._id },
   });
 });
 
 requestSchema.post("findOneAndUpdate", async function (doc) {
   if (this._update.status === "accepted") {
-    await User.findByIdAndUpdate(doc.sender, {
+    await model("User").findByIdAndUpdate(doc.sender, {
       $addToSet: { friends: doc.recipient },
     });
-    await User.findByIdAndUpdate(doc.recipient, {
+    await model("User").findByIdAndUpdate(doc.recipient, {
       $addToSet: { friends: doc.sender },
     });
   }
   // pre-delete hook to remove request ID from recipient's requests array
-  await Request.findByIdAndDelete(doc._id);
+  await model("Request").findByIdAndDelete(doc._id);
 });
 
 requestSchema.pre(
   "findOneAndDelete",
   { document: true },
   async function (next) {
-    await User.findByIdAndUpdate(this.recipient, {
+    await model("User").findByIdAndUpdate(this.recipient, {
       $pull: { requests: this._id },
     });
 
@@ -59,7 +58,7 @@ requestSchema.pre("deleteMany", async function (next) {
     };
   });
 
-  await User.bulkWrite(updateOperations);
+  await model("User").bulkWrite(updateOperations);
 
   next();
 });
