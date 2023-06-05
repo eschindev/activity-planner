8214b02ee86f5dfd94e1bd17f0f57f3bbe0e9291
osuchaya <suchaya.osatis@gmail.com>
2023-06-04 22:04:23 -0700

    Changed Checkbox to Select MenuItem for Private activity

---
+++ client/src/components/InviteList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import InviteCard from './InviteCard';
+import Container from '@mui/material/Container';
+
+InviteList () => {
+    return ()
+};
+
+
+export default InviteList;
