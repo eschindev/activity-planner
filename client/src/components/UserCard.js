6792a49fa92fff8022e087938b0d15057f10dff0
kenneth o cruz <cruzkenneth504@gmail.com>
2023-06-06 13:08:28 -0700

    centered user cards

--- client/src/components/UserCard.js
+++ client/src/components/UserCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
@@ -7,16 +8,30 @@ import "../style/userCardStyle.css";
 
 export default function UserCard({ user }) {
   return (
-    <CardContent className="user-content">
-      <AccountBoxIcon className="custom-icon" style={{ fontSize: 60 }} />
-      <Link to={`/user/${user._id}`}>
-        <Typography variant="h5" component="div">
-          {user.username}
-        </Typography>
-      </Link>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        {user.fullName}
-      </Typography>
-    </CardContent>
+    <div className="user-container">
+      <motion.div
+        className="user-content"
+        initial={{ scale: 0 }}
+        animate={{ rotate: 0, scale: 1 }}
+        transition={{
+          type: "spring",
+          stiffness: 260,
+          damping: 20
+        }}
+      >
+
+        <CardContent>
+          <AccountBoxIcon className="custom-icon" style={{ fontSize: 60 }} />
+          <Link to={`/user/${user._id}`}>
+            <Typography variant="h5" component="div">
+              {user.username}
+            </Typography>
+          </Link>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            {user.fullName}
+          </Typography>
+        </CardContent>
+      </motion.div>
+    </div>
   );
 }
