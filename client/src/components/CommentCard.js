3c412259fd06efcabf5eefb5db18af99fb3bba7c
osuchaya <suchaya.osatis@gmail.com>
2023-06-06 11:37:16 -0700

    commentcard code added

--- client/src/components/CommentCard.js
+++ client/src/components/CommentCard.js
@@ -1,22 +1,88 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
 
-export default function CommentCard({ comment }) {
+import { ADD_COMMENT } from '../../utils/mutations';
+
+import Auth from '../../utils/auth';
+
+const CommentCard = ({ commentId }) => {
+  const [commentText, setCommentText] = useState('');
+  const [characterCount, setCharacterCount] = useState(0);
+
+  const [addComment, { error }] = useMutation(ADD_COMMENT);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await addComment({
+        variables: {
+          activityId,
+          commentBody,
+          username: Auth.getProfile().data.username,
+        },
+      });
+
+      setCommentText('');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    if (name === 'commentText' && value.length <= 280) {
+      setCommentText(value);
+      setCharacterCount(value.length);
+    }
+  };
+
   return (
-    <Card sx={{ margin: "10px" }}>
-      <CardContent>
-        <Link to={`/user/${user._id}`}>
-          <Typography variant="h5" component="div">
-            {user.username}
-          </Typography>
-        </Link>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          {user.fullName}
-        </Typography>
-      </CardContent>
-    </Card>
+    <div>
+      <h4>What are your thoughts on this thought?</h4>
+
+      {Auth.loggedIn() ? (
+        <>
+          <p
+            className={`m-0 ${
+              characterCount === 280 || error ? 'text-danger' : ''
+            }`}
+          >
+            Character Count: {characterCount}/280
+            {error && <span className="ml-2">{error.message}</span>}
+          </p>
+          <form
+            className="flex-row justify-center justify-space-between-md align-center"
+            onSubmit={handleFormSubmit}
+          >
+            <div className="col-12 col-lg-9">
+              <textarea
+                name="commentText"
+                placeholder="Add your comment..."
+                value={commentText}
+                className="form-input w-100"
+                style={{ lineHeight: '1.5', resize: 'vertical' }}
+                onChange={handleChange}
+              ></textarea>
+            </div>
+
+            <div className="col-12 col-lg-3">
+              <button className="btn btn-primary btn-block py-3" type="submit">
+                Add Comment
+              </button>
+            </div>
+          </form>
+        </>
+      ) : (
+        <p>
+          You need to be logged in to share your thoughts. Please{' '}
+          <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
+        </p>
+      )}
+    </div>
   );
-}
+};
+
+export default CommentCard;
