86a3ec1f9614e44ff0d64ce618f1ee38058e301e
osuchaya <suchaya.osatis@gmail.com>
2023-06-06 06:56:49 -0700

    Debugged and fixed Add Friend error and alert if already friend

--- client/src/components/FriendRequestList.js
+++ client/src/components/FriendRequestList.js
@@ -6,15 +6,15 @@ import { UPDATE_REQUEST } from "../utils/mutations";
 import Button from "@mui/material/Button";
 import FriendRequestCard from "./FriendRequestCard";
 
-const FriendRequestList = ({ requests }) => {
-
+const FriendRequestList = ( {requests} ) => {
+console.log("here is:", requests);
 
   return (
     <div>
       <h2>Friend Requests</h2>
       <ul>
         {requests.map((request) => (
-          <li key={request._id} reqId={request._id}> {/*Do NOT get rid of this key when formatting!!! */}
+          <li key={request._id} reqId={request._id}>
             <FriendRequestCard request={request}/>
           </li>
         ))}
