598f124d29caa8144c44918b435be3e70177cab0
eschindev <118208710+eschindev@users.noreply.github.com>
2023-06-08 13:49:31 -0700

    removed extra message and unused variables

--- client/src/components/RequestList.js
+++ client/src/components/RequestList.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import RequestCard from "./RequestCard";
 import Container from "@mui/material/Container";
-import "../style/requestListCard.css"
-import "../style/notFound.css"
+import "../style/requestListCard.css";
+import "../style/notFound.css";
 
 export default function RequestList({ requests }) {
   return (
-    <Container maxWidth="sm"
-    style={{ maxHeight: "300px", overflow: "auto" }}
-    className="request-list-container"
+    <Container
+      maxWidth="sm"
+      style={{ maxHeight: "300px", overflow: "auto" }}
+      className="request-list-container"
     >
       {requests ? (
         requests.map((request) => {
@@ -17,8 +17,8 @@ export default function RequestList({ requests }) {
         })
       ) : (
         <div className="not-found-container">
-        <h1 className="text">No requests found</h1>
-      </div>
+          <h1 className="text">No requests found</h1>
+        </div>
       )}
     </Container>
   );
