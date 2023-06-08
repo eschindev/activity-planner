d3dec92617acdd4b15710cd561f326ef10b7362d
kenneth o cruz <cruzkenneth504@gmail.com>
2023-06-07 17:46:05 -0700

    styles profile page

--- client/src/pages/MyProfilePage.js
+++ client/src/pages/MyProfilePage.js
@@ -8,7 +8,11 @@ import ActivityList from "../components/ActivityList";
 import UserList from "../components/UserList";
 import InviteList from "../components/InviteList";
 import RequestList from "../components/RequestList";
-import "../style/myProfile.css"
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+
+import "../style/myProfile.css";
 
 const MyProfilePage = () => {
   if (!auth.loggedIn()) {
@@ -23,7 +27,14 @@ const MyProfilePage = () => {
   const { loading, data } = useQuery(QUERY_ME);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={true}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    );
   }
 
   const user = data?.getMyUser || {};
@@ -50,22 +61,33 @@ const MyProfilePage = () => {
     <Grid container spacing={2}>
       <Grid item xs={12}>
         <Grid container spacing={0}>
-          <Grid item xs={12} md={8}>
-            <Typography variant="h2">{user.username}</Typography>
-            <Typography variant="h5">{user.fullName}</Typography>
+          <Grid className="header-container" item xs={12} md={9}>
+            <AccountCircleIcon
+              className="account-circle-icon"
+              sx={{ fontSize: "150px" }}
+            />
+
+            <div class="content-container">
+              <Typography className="user-name" variant="h3">
+                {user.username}
+              </Typography>
+              <Typography className="full-name" variant="h5">
+                {user.fullName}
+              </Typography>
+            </div>
           </Grid>
           <Grid item xs={12} md={4}>
             <Button
               variant="contained"
               onClick={createActivity}
-              sx={{ margin: "20px" }}
+              sx={{ margin: "20px", ml: 1.0, mb:"0" }}
             >
               Create Activity
             </Button>
             <Button
               variant="contained"
               onClick={handleDeleteAccount}
-              sx={{ margin: "20px", backgroundColor: "red" }}
+              sx={{ margin: "20px",  mb: 0, ml: 1.0, backgroundColor: "red" }}
             >
               Delete Account
             </Button>
@@ -73,19 +95,43 @@ const MyProfilePage = () => {
         </Grid>
       </Grid>
       <Grid item xs={12} lg={6}>
-        <Typography className="header-style" sx={{display: "flex", justifyContent:"center", mg: 2.0}} variant="h4">Activities:</Typography>
+        <Typography
+          className="header-style"
+          sx={{ display: "flex", justifyContent: "center", mg: 2.0 }}
+          variant="h4"
+        >
+          Activities:
+        </Typography>
         <ActivityList activities={user.activities} />
       </Grid>
       <Grid item xs={12} lg={6}>
-        <Typography className="header-style" sx={{display: "flex", justifyContent:"center", mg: 2.0}} variant="h4">Friends:</Typography>
+        <Typography
+          className="header-style"
+          sx={{ display: "flex", justifyContent: "center", mg: 2.0 }}
+          variant="h4"
+        >
+          Friends:
+        </Typography>
         <UserList users={user.friends} />
       </Grid>
       <Grid item xs={12} lg={6}>
-        <Typography className="header-style" sx={{display: "flex", justifyContent:"center", mg: 2.0}} variant="h4">Activity Invites:</Typography>
+        <Typography
+          className="header-style"
+          sx={{ display: "flex", justifyContent: "center", mg: 2.0 }}
+          variant="h4"
+        >
+          Activity Invites:
+        </Typography>
         <InviteList invites={user.invites} />
       </Grid>
       <Grid item xs={12} lg={6}>
-        <Typography className="header-style" sx={{display: "flex", justifyContent:"center", mg: 2.0}} variant="h4">Friend Requests:</Typography>
+        <Typography
+          className="header-style"
+          sx={{ display: "flex", justifyContent: "center", mg: 2.0 }}
+          variant="h4"
+        >
+          Friend Requests:
+        </Typography>
         <RequestList requests={user.requests} />
       </Grid>
     </Grid>
